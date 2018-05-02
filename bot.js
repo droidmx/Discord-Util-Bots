ffaeae2266bd17070f4257ec6d721745704f8e96
Sreyaash <33704504+droidmx@users.noreply.github.com>
2018-05-01 23:22:59 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,8 +16,8 @@ bot.on('message', message => {
   	}
 });
 bot.on('message', message => {
-    if (message.content === 'test') {
-    	message.reply('working, active');
+    if (message.content === '!headcount') {
+    	bot.channels.get('433789873690902532').send("@here a headcount is starting! React with :Shatters: if you wish to participate, and :Key: if you have a key!")
     }
 });
 
