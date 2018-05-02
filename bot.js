80b9140044c476aedc2ffe84ccbb1bfa504a715a
Sreyaash <33704504+droidmx@users.noreply.github.com>
2018-05-02 00:17:53 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,7 +13,7 @@ client.on('message', message => {
 
 client.on('message', message => {
     if (message.content === '!headcount') {
-    	bot.channels.get('433789873690902532').send("@here a Headcount is starting! React with :Shatters: to participate and :Key: if you have a key and are willing to pop!");
+    	client.channels.get('433789873690902532').send("@here a Headcount is starting! React with :Shatters: to participate and :Key: if you have a key and are willing to pop!");
   	}
 });
 
