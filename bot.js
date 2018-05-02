9d7aecd53cc32f4d5cb2b5a54b9fe383d5ae13c7
Sreyaash <33704504+droidmx@users.noreply.github.com>
2018-05-02 18:57:28 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,8 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const snekfetch = require("snekfetch");
 
+var user = args.slice(0).join("");
+
 client.on('ready', () => {
     console.log('I am ready!');
 });
