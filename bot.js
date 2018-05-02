3cb5fb465df4be1660bc4a902c870ec0c137d206
Sreyaash <33704504+droidmx@users.noreply.github.com>
2018-05-02 01:29:26 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,7 +19,8 @@ client.on('message', message => {
 
 client.on('message', message => {
     if (message.content === '!afk') {
-        var new = client.channels.get('441093729290289152').send("@here afk check testing");
+        message.reply('testing');
+        // var new = client.channels.get('441093729290289152').send("@here afk check testing");
         //new.react(message.guild.emojis.get('433791162411646988'));
        
   	}
