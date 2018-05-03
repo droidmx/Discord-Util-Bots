5e783d7eeca7889e346580048d16a5677d8785b6
Sreyaash <33704504+droidmx@users.noreply.github.com>
2018-05-02 22:38:47 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,5 +5,63 @@ const prefix = '!';
 const ownerid =  "368756694114893825"
 const snekfetch = require("snekfetch");
 
+case "!verify":
+let ruser = args.slice(0).join("");
+let rcode = ("CH" + Math.floor(Math.random(11111) * 99999));
+let rapi = "http://www.tiffit.net/RealmInfo/api/user?u=" + ruser + "&f=c;"
+
+snekfetch.get(rapi).then(h => {
+  let brdesc = h.body.description;
+
+if(!ruser)
+return message.author.send("Please include a username after !verify! Any typos will cause your verification process to fail.")
+
+message.delete();
+
+message.author.send({embed: {
+  color: 0xff040b,
+  author: {
+    name: `Verification | ${message.author.tag}`,
+    icon_url: message.author.avatarURL
+  },
+  fields: [{
+      name: "**Your Code:**",
+      value: `__**${rcode}**__`,
+      inline: true,
+    },
+    {
+      name: "**Realmeye Link:**",
+      value: `https://www.realmeye.com/player/${ruser}`,
+      inline: true,
+    },
+    {
+      name: `Place your code in the __**first line**__ of your Realmeye description, replacing everything else.`,
+      value: `Your original Realmeye description will be sent back shortly.`,
+    },
+  ],
+  footer: {
+    text: "*The bot will check in 90 seconds to see if you followed directions.*",
+  }
+}
+});
+
+setTimeout(function(){ 
+
+snekfetch.get(rapi).then(r => {
+  let rdesc = r.body.description;
+  let rname = r.body.name
+
+  if(!rdesc.includes(rcode))
+  return message.author.send("Your code was not found in the first line of your Realmeye description. Your previous Realmeye description was:\n```" + brdesc + "```")
+
+  if(rdesc.includes(rcode))
+  message.guild.member(message.author).setNickname(`${rname}`)
+  message.guild.member(message.author).addRole("437853950033526785")
+  message.author.send("You have successfully been verified!\nYour previous Realmeye description was:\n```" + brdesc + "```");
+})
+}, 60000);
+})
+break;
+
 
 client.login(process.env.BOT_TOKEN);
