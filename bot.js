94cf03d6d3efed3b4a83a81384b2401022167dcc
Sreyaash <33704504+droidmx@users.noreply.github.com>
2018-05-02 21:30:13 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,24 +2,63 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const snekfetch = require("snekfetch");
 
-let api = "http://www.tiffit.net/RealmInfo/api/user?u=" + ruser + "&f=c;"
+case "!verify":
+let ruser = args.slice(0).join("");
+let rcode = ("DROID" + Math.floor(Math.random(11111) * 99999));
+let rapi = "http://www.tiffit.net/RealmInfo/api/user?u=" + ruser + "&f=c;"
 
-var user = args.slice(0).join("");
+snekfetch.get(rapi).then(h => {
+  let brdesc = h.body.description;
 
-let rcode = ("SC" + Math.floor(Math.random(11111) * 99999));
+if(!ruser)
+return message.author.send("Please include a username after !verify! Any typos will cause your verification process to fail.")
 
-client.on('message', function(message) {
-    var args = message.content.split(" ");
-    var cmd = args[0];
-    var user = args.slice(0).join("");
-  
-    args = args.splice(1);
-  
-         switch(cmd) {
-                 case "!headcount":
-client.channels.get('433789873690902532').send("@here Headcount! React with <:Shatters:433791162411646988> to participate and <:Key:434134124631031810> if you have a key and are willing to pop!")
+message.delete();
+
+message.author.send({embed: {
+  color: 0xff040b,
+  author: {
+    name: `Verification | ${message.author.tag}`,
+    icon_url: message.author.avatarURL
+  },
+  fields: [{
+      name: "**Your Code:**",
+      value: `__**${rcode}**__`,
+      inline: true,
+    },
+    {
+      name: "**Realmeye Link:**",
+      value: `https://www.realmeye.com/player/${ruser}`,
+      inline: true,
+    },
+    {
+      name: `Place your code in the __**first line**__ of your Realmeye description, replacing everything else.`,
+      value: `Your original Realmeye description will be sent back shortly.`,
+    },
+  ],
+  footer: {
+    text: "*The bot will check in 60 seconds to see if you followed directions.*",
+  }
+}
+});
+
+setTimeout(function(){ 
+
+snekfetch.get(rapi).then(r => {
+  let rdesc = r.body.description;
+  let rname = r.body.name
+
+  if(!rdesc.includes(rcode))
+  return message.author.send("Your code was not found in the first line of your Realmeye description. Your previous Realmeye description was:\n```" + brdesc + "```")
+
+  if(rdesc.includes(rcode))
+  message.guild.member(message.author).setNickname(`${rname}`)
+  message.guild.member(message.author).addRole("441409817232539678")
+  message.author.send("You have successfully been verified!\nYour previous Realmeye description was:\n```" + brdesc + "```");
+})
+}, 60000);
+})
 break;
-         }
 
 client.on('ready', () => {
     console.log('I am ready!');
@@ -43,19 +82,6 @@ client.on('message', message => {
   	}
 });
 
-client.on('message', message => {
-    if (message.content === '!afk') {
-        
-         client.channels.get('441093729290289152').send("@here We are starting an afk check now, join queue and react with <:Shatters:433791162411646988> to be moved in!").then(newMessage => {
-             
-        newMessage.react(message.guild.emojis.get('433791162411646988'))
-             .catch(console.error);
-         })
- 
-         
-       
-  	}
-});
 
 
 // THIS  MUST  BE  THIS  WAY
