dffe553a435cb8e30c254b287422f5ee47f1a094
Sreyaash <33704504+droidmx@users.noreply.github.com>
2018-05-12 00:14:34 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,59 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const snekfetch = require("snekfetch");
 
+const fs = require('fs');
+let XP = JSON.parse(fs.readFileSync('./XP.json', 'utf8'));
+
+client.on("message", msg => {
+	let prefix = "!";
+	
+	console.log(0)
+	if(!msg.content.startsWith(prefix)) return;
+	
+	//console.log(0.1)
+	//if(msg.author.id != "Your ID") return;
+	//Only use the above for testing as only the person with that ID can use the bot.
+	
+	
+	
+	let userData = XP[msg.author.id];
+	if (!userData) userData = {XP: 0, level: 0};
+	
+	let userXP = XP[msg.author.id] ? XP[msg.author.id].XP : 0;
+	let curLevel = Math.floor(0.1 * Math.sqrt(userXP));
+	if (curLevel > userData.level) {
+		userData.level = curLevel;
+		msg.reply(`You have lvled ^ to lvl **${curLevel}**!`);
+	}
+	
+	console.log("level")
+	if (msg.content.startsWith(prefix + "level")) {
+		msg.reply(`You are lvl ${userData.level}, with ${userData.XP} XP Right Now.`);
+	}
+	
+	if (!XP[msg.author.id]) XP[msg.author.id] = {XP: 0, level: 0}
+	
+	
+	
+	console.log("Slime")
+	if (msg.content.startsWith(prefix + "killSlime")) {
+		userData.XP += 10
+		msg.channel.sendMessage(`${msg.author} has killed an Slime!`)
+	}
+  
+  	console.log("Oryx")
+	if (msg.content.startsWith(prefix + "killOryx")) {
+		userData.XP += 20
+		msg.channel.sendMessage(`${msg.author} has killed Oryx the Mad God!!`)
+	}
+	
+	console.log(XP)
+	fs.writeFile('./XP.json', JSON.stringify(XP), console.error);
+	
+});
+
+
+
 //line 123 is where lhgs specific starts. 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
