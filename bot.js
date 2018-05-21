1246972a1523ddf844658582c6aa76d561ea2ba3
Sreyaash <33704504+droidmx@users.noreply.github.com>
2018-05-21 17:39:21 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -68,7 +68,149 @@ client.on('raw', event => {
 });
 
 
-//line 123 is where lhgs specific starts. 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setPresence({ game: { name: `with unicorns | >>help`, type: 0 }});
+});
+
+const prefix = ">>"
+
+const answers = [
+  'Without a doubt', 'Extremely likely', 'Perhaps', 'Maybe', 'I\'ll have to think about that', 'Not a chance!'
+]
+
+client.on('message', msg => { // START MESSAGE HANDLER
+  if (msg.author.bot) return;
+let args = msg.content.split(" ").slice(1);
+  if (msg.content.startsWith(prefix + 'ping')) {
+    msg.channel.send("Pinging... :signal_strength:").then(sent => {
+      sent.edit(`:ping_pong: Pong! | Time Taken: ${sent.createdTimestamp - msg.createdTimestamp}ms`)
+    })
+  }
+
+  if (msg.content.startsWith(prefix + '8ball')) {
+  let args = msg.content.split(" ").slice(1);
+  let question = args[0]
+  if (!msg.content.endsWith('?')) {
+    return msg.channel.send('You must ask me a question first!')
+} else {
+  msg.channel.send(`:8ball: | ${answers[Math.floor(Math.random() * answers.length)]}`);
+  }
+}
+  
+  if (msg.content.startsWith(prefix + "serverinfo")) {
+    const embed = new Discord.RichEmbed()
+
+    .setTitle(`${msg.guild.name}`)
+    .setColor(0x17bec6)
+    .addField(`Owner`, `${msg.guild.owner.user.tag} (${msg.guild.owner.id})`)
+    .addField(`Members`, `${msg.guild.memberCount}`)
+    .addField(`Region`, `${msg.guild.region}`)
+    .addField(`ID`, `${msg.guild.id}`)
+    .addField(`Channels`, `${msg.guild.channels.size}`)
+    .addField(`Created at`, `Created at date: WIP`)
+
+    msg.channel.send({embed});
+}
+  
+  if (msg.content.startsWith(prefix + 'help')) {
+  msg.channel.send(":inbox_tray: | Listed below are some commands")
+  msg.channel.send(`\`\`\`asciidoc
+= General =
+>>ping :: Hm. I wonder what this does? /sarcasm
+>>8ball :: Ask the magic 8ball a question. Pretty self explanatory
+>>help :: Brings up this menu
+>>invite :: Shows HeatZone's Invite URL
+>>serverinfo :: Shows information about the server\`\`\``)
+  msg.channel.send(`\`\`\`asciidoc
+= Moderation =
+>>ban :: Bans the user specified
+>>kick :: Kicks the user specified
+>>softban :: Softbans the specified user\`\`\``)
+  }
+
+  
+
+  if (msg.content.startsWith(prefix + 'ban')) {
+    var reason = msg.content.split(' ').slice(2).join(' ');
+    if (!msg.member.permissions.has("BAN_MEMBERS")) return msg.channel.send(":warning: Insufficient Permissions").catch(console.error);
+    if (!msg.guild.member(client.user).permissions.has("BAN_MEMBERS")) return msg.channel.send(":warning: Bot has insufficient permissions").catch(console.error);
+
+  if (msg.mentions.users.size === 0) return msg.channel.send("No user provided")
+
+  if (msg.author.id === msg.mentions.members.first().user.id) return msg.channel.send("You can't ban yourself").catch(console.error);
+
+  if (client.user.id === msg.mentions.users.first().id) return msg.channel.send(`Don't try to ban me, ${msg.author.username}`).catch(console.error);
+
+  userToBan.ban()
+    var user = msg.mentions.users.first()
+    const embed = new Discord.RichEmbed()
+    .setTitle(`:hammer: User Banned: ${user.tag} (${user.id})`)
+    .setColor(0xd11212)
+    .addField("Responsible Moderator:", `${msg.author.tag} (${msg.author.id})\n\nReason: ${reason}`)
+    .setTimestamp(new Date(msg.createdTimestamp))
+
+    msg.guild.channels.find("name", "logging").send({embed});
+  }
+
+  if (msg.content.startsWith(prefix + 'kick')) {
+    var reason = msg.content.split(' ').slice(2).join(' ');
+    if (!msg.member.permissions.has("KICK_MEMBERS")) return msg.channel.send(":warning: Insufficient Permissions").catch(console.error);
+    if (!msg.guild.member(client.user).permissions.has("KICK_MEMBERS")) return msg.channel.send(":warning: Bot has insufficient permissions").catch(console.error);
+
+    if (msg.mentions.users.size === 0) return msg.channel.send("No user provided")
+
+    if (!msg.guild.member(userToKick).kickable) return msg.channel.send("I can't kick that member!")
+
+    if (msg.author.id === msg.mentions.members.first().user.id) return msg.channel.send("You can't kick yourself");
+
+    if (client.user.id === msg.mentions.users.first().id) return msg.channel.send(`Don't try to kick me, ${msg.author.username}`).catch(console.error);
+
+  userToKick.kick()
+    var user = msg.mentions.users.first()
+    const embed = new Discord.RichEmbed()
+    .setTitle(`:hammer: User Kicked: ${user.tag} (${user.id})`)
+    .setColor(0xf9a411)
+    .addField("Responsible Moderator:", `${msg.author.tag} (${msg.author.id})\n\nReason: ${reason}`)
+    .setTimestamp(new Date(msg.createdTimestamp))
+
+    msg.guild.channels.find("name", "logging").send({embed});
+  }
+
+  
+if (msg.content.startsWith(prefix + 'info')) {
+  client.channel.send("Bot coded by Droid")
+}
+  
+  
+ if (msg.content.startsWith(prefix + 'softban')) {
+   var reason = msg.content.split(' ').slice(2).join(' ');
+   if (!msg.member.permissions.has("BAN_MEMBERS")) return msg.channel.send(":warning: Insufficient Permissions").catch(console.error);
+   if (!msg.guild.member(client.user).permissions.has("BAN_MEMBERS")) return msg.channel.send(":warning: Bot has insufficient permissions").catch(console.error);
+
+     if (msg.mentions.users.size === 0) return msg.channel.send("No user provided")
+
+     if (!msg.guild.member(userToSB).bannable) return msg.channel.send("I can't softban that member!").catch(console.error);
+
+     if (msg.author.id === msg.mentions.members.first().user.id) return msg.channel.send("You can't softban yourself").catch(console.error);
+
+     if (client.user.id === msg.mentions.users.first().id) return msg.channel.send(`Don't try to softban me, ${msg.author.username}`).catch(console.error);
+
+   userToSB.ban().then(member => {msg.guild.unban(member.user.id)});
+     var user = msg.mentions.users.first()
+     const embed = new Discord.RichEmbed()
+     .setTitle(`:hammer: User Softbanned: ${user.tag} (${user.id})`)
+     .setColor(0xfffa00)
+     .addField("Responsible Moderator:", `${msg.author.tag} (${msg.author.id})\n\nReason: ${reason}`)
+     .setTimestamp(new Date(msg.createdTimestamp))
+
+     msg.guild.channels.find("name", "logging").send({embed});
+  }
+
+});
+    
+
+//line 123 is wher
 -
 -
 -
