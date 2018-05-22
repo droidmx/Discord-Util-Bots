107aaf83f7e2f089adb67e19385cb59d1ebb90a7
Sreyaash <33704504+droidmx@users.noreply.github.com>
2018-05-22 01:04:26 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -136,8 +136,8 @@ let args = msg.content.split(" ").slice(1);
 		let urmom = XP[i]
 		
 		let asd = urmom.level
-		let boii = urmom.XP
-		.addField("__**User**__", `<@${i}> is level ${asd} and has ${boii} XP`, true);
+		//let boii = urmom.XP
+		.addField("__**User**__", `<@${i}> is level ${asd} and has test XP`, true);
 			
 		
 		
