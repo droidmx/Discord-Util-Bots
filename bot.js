7def93822113635700e565114b9bdb88f30adf13
Sreyaash <33704504+droidmx@users.noreply.github.com>
2018-05-22 03:16:50 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -99,7 +99,7 @@ let args = msg.content.split(" ").slice(1);
 		msg.reply(`You have lvled ^ to lvl **${curLevel}**!`);
 	}
 	 if (talkedRecently.has(msg.author.id)) {
-            msg.channel.send("Wait 5 seconds before getting typing this again. - " + msg.author);
+            msg.channel.send("Wait 5 seconds before typing this again. - " + msg.author);
     } else {
 	console.log("level")
 	if (msg.content.startsWith(prefix + "level")) {
