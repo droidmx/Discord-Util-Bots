51a43240f30d99dbfdca53aec83061312126fbe7
Sreyaash <33704504+droidmx@users.noreply.github.com>
2018-05-31 22:44:24 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -108,7 +108,7 @@ client.on('guildMemberAdd', member => {
 client.on('message', msg => {
     if (msg.content.startsWith('!test')) {
         var argss = msg.content.split(" ");
-        let userdata = test[msg.author.id]
+        
         let ruser = argss[1]
             let rcode = ("SC" + Math.floor(Math.random(11111) * 99999));
 let rapi = "http://www.tiffit.net/RealmInfo/api/user?u=" + ruser + "&f=c;"
