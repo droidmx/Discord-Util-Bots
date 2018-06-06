820af3442a70f6723d0e9c0300906660ba614cca
Sreyaash <33704504+droidmx@users.noreply.github.com>
2018-06-05 23:25:21 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -162,9 +162,9 @@ client.on('message', function(message) {
                       .addField("__**Current Requirements**__", "As of now, the current requirements are 200 Alive Fame, and either one 8/8 or 2 6/8's or 7/8's.")
                       .addField("__**Character Information**__", `${person} has ${sixeight} 6/8's, ${7/8} 7/8's and ${eighteight} 8/8's`, true)
                       .addField("__**Fame Information**__", `${person} has ${personfame} Alive Fame, and has ${personstars} Stars!`, true)
-                      .addField("__**Requirements**__", `***${person} ${meetsreqs} meet the current requirements!***`)
+                      .addField("__**Requirements**__", `***${person} ${meetsreqs} meet the current requirements!***`);
                       
-                      message.channel.send({finalmessage})
+                      message.channel.send({finalmessage});
                       
                       
                     }
