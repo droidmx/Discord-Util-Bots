acc6bf474752e136df6bdb55f76db114ff3a80b4
Sreyaash <33704504+droidmx@users.noreply.github.com>
2018-06-13 00:07:12 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,12 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+let users = JSON.parse(fs.readFileSync('./users.json', 'utf8'));
 
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
-
+const prefix = '>>'
 function wait(ms){
    var start = new Date().getTime();
    var end = start;
@@ -14,7 +15,9 @@ function wait(ms){
   }
 }
 client.on('message', async message => {
-   
+   if (!message.content.startsWith(prefix)) {
+       
+   }
 });
 
 
