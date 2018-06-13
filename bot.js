d807f69aabe736b6a2bb1134ea5ae983702d6cca
Sreyaash <33704504+droidmx@users.noreply.github.com>
2018-06-13 12:26:44 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -378,25 +378,20 @@ client.on('message', async msg => { //start message handler
         console.log(sorting)
         console.log(retrieval)
         sorting.sort((a, b) => b - a);
-        var message = `
-        **1)** <@${retrieval[sorting[0]]}> with $${sorting[0]}\n
-        **2)** <@${retrieval[sorting[1]]}> with $${sorting[1]}\n
-        **3)** <@${retrieval[sorting[2]]}> with $${sorting[2]}\n
-        **4)** <@${retrieval[sorting[3]]}> with $${sorting[3]}\n
-        **5)** <@${retrieval[sorting[4]]}> with $${sorting[4]}
+        var message = `**1)** <@${retrieval[sorting[0]]}> with $${sorting[0]}\n**2)** <@${retrieval[sorting[1]]}> with $${sorting[1]}\n**3)** <@${retrieval[sorting[2]]}> with $${sorting[2]}\n**4)** <@${retrieval[sorting[3]]}> with $${sorting[3]}\n**5)** <@${retrieval[sorting[4]]}> with $${sorting[4]}
         `
-        
+
         msg.channel.send({
-                embed: {
-                    color: 000000,
-                    author: {
-                        name: msg.author.username,
-                        icon_url: msg.author.avatarURL
-                    },
-                    title: "The Top 5 users are: ",
-                    description: message
-                }
-            });
+            embed: {
+                color: 000000,
+                author: {
+                    name: msg.author.username,
+                    icon_url: msg.author.avatarURL
+                },
+                title: "The Top 5 users are: ",
+                description: message
+            }
+        });
     }
 
 }); //end message handler
