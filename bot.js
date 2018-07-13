54e3d07a922f26fa28cebd57c06339e40559dc21
Sreyaash <33704504+droidmx@users.noreply.github.com>
2018-07-13 17:15:31 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,5 @@
+//
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -5,11 +7,7 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
-});
+
 function wait(ms){
    var start = new Date().getTime();
    var end = start;
@@ -17,7 +15,24 @@ function wait(ms){
      end = new Date().getTime();
   }
 }
-client.on('message', async message => {
+client.on('message', async msg => {
+if (!msg.author.bot) return;
+console.log(msg.content)
+
+if (msg.channel.id == '379779029479194624') {
+console.log(msg.content)
+}
+
+if (msg.channel.id == '399206894125973525') {
+console.log(msg.content)
+}
+
+if (msg.channel.id == '448683172306354176') {
+console.log(msg.content)
+}
+if (msg.channel.id == '448683172306354176') {
+console.log(msg.content)
+}
    
 });
 
