5990714344a21c84c7fe61fa49cf8d96a2af2796
Sreyaash <33704504+droidmx@users.noreply.github.com>
2018-07-17 20:18:50 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -25,7 +25,7 @@ client.on('message', async msg => {
         var shatname = msg.embeds[0].author.name
         var shatargs = shatname.split(" ");
         var shatrl = shatargs[0]
-        client.channels.get('467445547557453837').send(`**<@&467420984916312064> AFK-Check**\` was started!\` | \`Raid Leader:\` **${shatrl}** | \`[${moment().format("LT")}]\``)
+        client.channels.get('467445547557453837').send(`**<@&467420984916312064> AFK-Check** \`was started!\` | \`Raid Leader:\` **${shatrl}** | \`[${moment().format("LT")}]\``)
     }
 
     //LHZ
@@ -33,7 +33,7 @@ client.on('message', async msg => {
         var lhzname = msg.embeds[0].author.name
         var lhzargs = lhzname.split(' ');
         var lhzrl = lhzargs[0]
-        client.channels.get('467445547557453837').send(`**<@&467421140877574165> AFK-Check**\` was started!\` | \`Raid Leader:\` **${lhzrl}** | \`[${moment().format("LT")}]\``)
+        client.channels.get('467445547557453837').send(`**<@&467421140877574165> AFK-Check** \`was started!\` | \`Raid Leader:\` **${lhzrl}** | \`[${moment().format("LT")}]\``)
     }
 
     //EDZ
@@ -41,7 +41,7 @@ client.on('message', async msg => {
         var edzname = msg.embeds[0].author.name
         var edzargs = edzname.split(' ');
         var edzrl = edzargs[0]
-        client.channels.get('467445547557453837').send(`**<@&468932208376414229> AFK-Check**\` was started!\` | \`Raid Leader:\` **${edzrl}** | \`[${moment().format("LT")}]\``)
+        client.channels.get('467445547557453837').send(`**<@&468932208376414229> AFK-Check** \`was started!\` | \`Raid Leader:\` **${edzrl}** | \`[${moment().format("LT")}]\``)
     }
 
     //PUB HALLS
@@ -51,7 +51,7 @@ client.on('message', async msg => {
         if (msg.content.includes('AFK check')) {
             var lhsmention = msg.mentions.users.last()
             var lhsrl = msg.guild.member(lhsmention).nickname
-            client.channels.get('467445547557453837').send(`**<@&467421029631787018> AFK-Check**\` was started!\` | \`Raid Leader:\` **${lhsrl}** | \`[${moment().format("LT")}]\``)
+            client.channels.get('467445547557453837').send(`**<@&467421029631787018> AFK-Check** \`was started!\` | \`Raid Leader:\` **${lhsrl}** | \`[${moment().format("LT")}]\``)
         }
 
     }
@@ -62,7 +62,7 @@ client.on('message', async msg => {
       var lbname = msg.embeds[0].title
       var lbargs = lbname.split(' ');
       var lbrl = lbargs[0].slice(1)
-        client.channels.get('467445547557453837').send(`**<@&468932303339388929> AFK-Check**\` was started!\` | \`Raid Leader:\` **${lbrl}** | \`[${moment().format("LT")}]\``)
+        client.channels.get('467445547557453837').send(`**<@&468932303339388929> AFK-Check** \`was started!\` | \`Raid Leader:\` **${lbrl}** | \`[${moment().format("LT")}]\``)
        
         }
     }
@@ -71,7 +71,7 @@ client.on('message', async msg => {
     //PUB SHATS
     if (msg.channel.id == '451181425115398184') {
         if (msg.content.includes('AFK Check')) {
-            client.channels.get('467445547557453837').send(`**<@&468933020343336961> AFK-Check**\` was started!\` | \`[${moment().format("LT")}]\``)
+            client.channels.get('467445547557453837').send(`**<@&468933020343336961> AFK-Check** \`was started!\` | \`[${moment().format("LT")}]\``)
         }
 
     }
