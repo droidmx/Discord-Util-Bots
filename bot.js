6ec1f70a93f3568b04d149a56cf46ea209cc5fc2
Sreyaash <33704504+droidmx@users.noreply.github.com>
2018-07-18 12:34:41 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,9 +3,15 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const moment = require('moment');
+const setupCMD = "!createrolemessage"
 
+let initialMessage = `**React to the messages below to receive the associated role. If you would like to remove the role, simply remove your reaction!**`;
+
+const roles = ["Pub Halls", "Shatters", "Pub Shats", "SBC", "LHZ", "EDZ", "Lost Boys"];
+
+const reactions = ["💻", "🖌", "😃", "🆕"];
 client.on('ready', () => {
-    console.log('I am ready!');
+    console.log(`Logged in as ${client.user.tag}`);
 });
 
 
@@ -17,6 +23,22 @@ function wait(ms){
   }
 }
 client.on('message', async msg => {
+if (msg.author.id == '368756694114893825') {
+if (msg.content.includes('/add')) {
+let guild = msg.guild;
+    let toAdd = args[2];
+    let title = args[1];
+
+    if(!title || !toAdd) {
+        return msg.channel.send('Please provide name and url for the emoji to add. Ex: >>addemoji <name> <url>');
+    } else {
+        guild.createEmoji(toAdd, title)
+        .then(emoji => msg.react(emoji))
+        .catch(console.log('Something went wrong.'));
+      
+    }
+}
+}
 if (msg.channel.id == '467445547557453837') {
  client.channels.get('467436171664949249').send(msg.content)       
 }
