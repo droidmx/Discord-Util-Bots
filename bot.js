9de2a5f70a7a3a15308e0770e4e2f3ab8abc1f3a
Sreyaash <33704504+droidmx@users.noreply.github.com>
2018-07-29 14:08:13 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,7 +26,7 @@ client.on('message', async msg => {
     if (!msg.author.bot) return;
 	if (msg.channel.id == '436281824583483402') {
 		console.log(msg.embeds)
-		console.log(msg.embeds[0].fields)
+		console.log(msg.embeds[0].fields[0])
 		//console.log(msg.toString)
 		//console.log(msg.mentions)
 		
