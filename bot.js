ca34e5cf9ae128bddef858aabe8837bc6a4c117b
Sreyaash <33704504+droidmx@users.noreply.github.com>
2018-07-30 17:36:44 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const moment = require('moment');
-
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}`);
+});
 client.on('message', async msg => {
 
   
