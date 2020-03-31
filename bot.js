2cc7b7fb23c5930864329bbf0c6fe5e62e95c9d0
Sreyaash <33704504+droidmx@users.noreply.github.com>
2020-03-30 22:55:24 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,24 +5,5 @@ const music = require('discord.js-musicbot-addon');
 client.on('message', msg => {
   if (msg.author.id != '368756694114893825') return;
 })
-music.start(client, {
-  youtubeKey: "AIzaSyAAKSiOkapv22Kt3OWNOAWeTcXJ0yLmJ90",
-  prefix: "-",
-  leaveAlt: ["fuckoff", "fuckingleave"],
-  helpCmd: "mhelp",
-  thumbnailType: "high",
-  maxQueueSize: 5,
-  enableQueueStat: true,
-  ownerOverMember: true,
-  clearOnLeave: true,
-  anyoneCanSkip: true,
-  anyoneCanPause: true,
-  logging: true,
-  requesterName: true,
-  embedColor: "RANDOM",
-  checkQueues: true,
-  pauseAlt: ['stop', 'wait'],
-  botOwner: "368756694114893825"
-});
 
 client.login(process.env.BOT_TOKEN);
