ab7e06a505f5cc46bf93a29fcad010456fc4c12c
Sreyaash <33704504+droidmx@users.noreply.github.com>
2020-03-30 23:00:40 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,21 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const music = require('discord.js-musicbot-addon');
+
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}`);
+    client.user.setActivity('the Nexus | /help', {
+            type: 'WATCHING'
+        }).then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : 'none'}`))
+        .catch(console.error);
+});
 
 client.on('message', msg => {
-  if (msg.author.id != '368756694114893825') return;
+  
+  if (msg.content.startsWith('bars')) {
+   msg.channel.send("BARS ON BARS ON BARS"); 
+    
+  }
 })
 
 client.login(process.env.BOT_TOKEN);
